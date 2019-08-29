1b5b0ac222b5233535b86b05ace01c472505ec43
Jacob Ogren <jogren0044@gmail.com>
2019-08-29 11:49:41 -0600

    Move all fetch calls to apiCalls file

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Container from '../Container/Container';
 import Form from '../Form/Form';
-import { getOrders } from '../apiCalls/apiCalls';
+import { getOrders, postOrder, deleteOrder } from '../apiCalls/apiCalls';
 import './App.css';
 
 class App extends Component {
@@ -21,21 +21,7 @@ class App extends Component {
 
   addPurchase = (e, formState) => {
     e.preventDefault();
-
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(formState),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    fetch('http://localhost:3001/api/v1/purchases', options)
-      .then(res => {
-        if(!res.ok) {
-          throw Error('Error posting order')
-        }
-        return res.json()
-      })
+    postOrder(formState)
       .then(newOrder => this.setState({ orders: [...this.state.orders, newOrder] }))
       .catch(error => console.log(error))
   }
@@ -43,13 +29,7 @@ class App extends Component {
   deletePurchase = id => {
     let filteredOrders = this.state.orders.filter(order => order.id !== id)
     this.setState({ orders: filteredOrders })
-    const options = {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    fetch(`http://localhost:3001/api/v1/purchases/${id}`, options)
+    deleteOrder(id);
   }
 
   render() {
--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -8,5 +8,31 @@ export const getOrders = () => {
     }).catch(error => {
       throw Error(error.message)
     })
+}
 
+export const postOrder = formState => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(formState),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost:3001/api/v1/purchases', options)
+    .then(res => {
+      if (!res.ok) {
+        throw Error('Error posting order')
+      }
+      return res.json()
+    })
+}
+
+export const deleteOrder = id => {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  fetch(`http://localhost:3001/api/v1/purchases/${id}`, options)
 }
