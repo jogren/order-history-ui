322fa5ba18df14663c8241c8080f36bca28daf42
Jacob Ogren <jogren0044@gmail.com>
2019-08-29 11:42:36 -0600

    Add async tests for getOrders 2 sad 1 happy

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,4 +1,12 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/purchases')
-    .then(res => res.json())
+    .then(res => {
+      if(!res.ok) {
+        throw Error('Error fetching your orders')
+      }
+      return res.json()
+    }).catch(error => {
+      throw Error(error.message)
+    })
+
 }
