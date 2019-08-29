e7d6ac4eec74b8ce8470980a519cabc6ae6d7d13
Jacob Ogren <jogren0044@gmail.com>
2019-08-29 09:21:29 -0600

    Fetch data successfully and setState

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      orders: [],
+      error: ''
+    }
+
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/purchases')
+      .then(res => res.json())
+      .then(data => this.setState({ orders: data }))
+      .catch(error => this.setState({ error: 'There was a problem fetching your data.' }))
+  }
+
   render() {
+    const { orders, error } = this.state;
+    console.log(orders)
+    console.log(error)
     return (
       <div className="App">
         <header>
